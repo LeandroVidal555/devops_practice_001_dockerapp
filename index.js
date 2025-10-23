1a2c43226602d9b17d4f1cf65a8d3f7288136514
Leandro Vidal <leandro.vidal@securitize.io>
2025-10-23 00:08:37 -0300

    fix app name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('πρόσταγμα?\n');
+  res.end('Prostagma?\n');
 });
 
 server.listen(port, () => {
