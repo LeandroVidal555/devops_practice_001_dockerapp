673b3e41328473e81f3a4aba0a7f38a379879819
Leandro Vidal <leandro.vidal@securitize.io>
2025-11-22 21:37:22 -0300

    add pretty logger and env conditional for local testing

--- logger.js
+++ logger.js
@@ -1,11 +1,18 @@
 const pino = require('pino');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: {
+  transport: isProd
+    ? undefined   // JSON logs in EKS only
+    : {
         target: 'pino-pretty',
-        options: { colorize: true, translateTime: 'SYS:standard' }
-  }
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard'
+        }
+      }
 });
 
 module.exports = logger;
